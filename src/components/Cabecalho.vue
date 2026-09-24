0337a44b6069455c0964fa869e35b944d0dc0c0f
Godoi <m4rkim0431@gmail.com>
2026-09-24 20:37:59 -0300

    adicionar variação de texto em cabeçalho

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -5,6 +5,7 @@
 <template>
     <header class="p-5 my-4 bg-light rondend-3">
             <h1>minhas Tarefas</h1>
-            <p>Você possui {{props.tarefasPendentes}} Tarefas pendentes</p>
+            <p v-if="props.tarefasPendentes > 0">Você possui {{props.tarefasPendentes}} Tarefas pendentes</p>
+            <p v-else> Você não tem tarefas pendentes</p>
     </header>
 </template>
